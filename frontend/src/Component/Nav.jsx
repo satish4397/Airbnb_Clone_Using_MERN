87e0d1f7992ecf0cbc2179ba7df296905df84715
Satish Chaurasiya <165125161+satish4397@users.noreply.github.com>
2026-01-15 21:11:40 +0530

    Updated Navbar

--- frontend/src/Component/Nav.jsx
+++ frontend/src/Component/Nav.jsx
@@ -70,7 +70,7 @@ function Nav() {
                     <button className='absolute p-[10px] rounded-[50px] bg-[red] right-[3%] top-[5px]'><FiSearch className='w-[20px] h-[20px] text-[white]' /></button>
                 </div>
                 <div className='flex items-center justify-center    gap-[10px] relative'>
-                  {/*  <span className='text-[18px] cursor-pointer rounded-[50px] hover:bg-[#ded9d9] px-[8px] py-[5px] hidden md:block' onClick={()=>navigate("/listingpage1")}>List your home</span> */}
+                           <span className='text-[18px] cursor-pointer rounded-[50px] hover:bg-[#ded9d9] px-[8px] py-[5px] hidden md:block' onClick={()=>navigate("/login")}>Login</span>
                     <button className='px-[20px] py-[10px] flex items-center justify-center gap-[5px] border-[1px] border-[#8d8c8c] rounded-[50px] hover:shadow-lg' onClick={()=>setShowpopup(prev =>!prev)}>
                         <span><GiHamburgerMenu className='w-[20px] h-[20px]' /></span>
                         {userData == null && <span><CgProfile className='w-[23px] h-[23px]' /></span>}
